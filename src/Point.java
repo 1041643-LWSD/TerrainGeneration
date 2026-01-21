f2a2064d3b4825e529797a416eb0137a661381d8
Aakash-Ghosh08 <aakash.kumar.ghosh@gmail.com>
2026-01-20 23:35:31 -0800

    Update Point.java

--- src/Point.java
+++ src/Point.java
@@ -1,4 +1,17 @@
-
 public class Point {
+    private double x;
+    private double y;
+    private double z;
 
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public boolean inBox(double xMin, double xMax, double yMin, double yMax, double zMin, double zMax) {
+        return (x >= xMin && x <= xMax) &&
+               (y >= yMin && y <= yMax) &&
+               (z >= zMin && z <= zMax);
+    }
 }
