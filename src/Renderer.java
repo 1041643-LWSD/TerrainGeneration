2350c5c8593c93b00b48959d51e4e3f8c455aee2
Aakash-Ghosh08 <aakash.kumar.ghosh@gmail.com>
2026-01-22 13:37:09 -0800

    Refactor Renderer resolution and add screen buffer
    Split RESOLUTION into xRESOLUTION and yRESOLUTION, and initialize a 2D screen buffer. Added renderScreen and updateScreen methods as placeholders, and corrected loop syntax in castRay.

--- src/Renderer.java
+++ src/Renderer.java
@@ -6,8 +6,10 @@ public class Renderer {
 	private double x;
 	private double y;
 	private double z;
-	private final double RESOLUTION = 120;
+	private final double xRESOLUTION = 120;
+	private final double yRESOLUTION = 120;
 	private final double FOV = 120;
+	private double[][] screen;
 	
 	public Renderer(NoiseMap terrain) {
 		this.terrain = terrain;
@@ -16,19 +18,24 @@ public class Renderer {
 		this.x = 0;
 		this.y = 0;
 		this.z = 0;	
+		screen = new double[(int)xRESOLUTION][(int)yRESOLUTION];
+	}
+
+	public void renderScreen() {
+
 	}
 	
-	public void render(double x, double y, double xDir, double yDir) {
-		
+	public void updateScreen(double x, double y, double xDir, double yDir) {
+
 	}
 	
 	public void castRay(double x, double y, double xDir, double yDir) {
 		Ray ray = new Ray(x, y, z, xDir, yDir);
-		for(int i = 0; i < terrain.getResolution()) {
-			for(int j = 0; j < terrain.getResolution()) {
+		for(int i = 0; i < terrain.getResolution(); i++) {
+			for(int j = 0; j < terrain.getResolution(); j++) {
 				double boxHeight = terrain.getHeightAt(i, j);
 				if(ray.collidingWithBox(i, i + 1, j, j + 1, boxHeight, boxHeight + 1)) {
-					//draw pixel
+					
 				}
 			}
 		}
