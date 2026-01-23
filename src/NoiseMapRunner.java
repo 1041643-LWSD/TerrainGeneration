b3263dbd3db32a37b1a644588ad45fa50d728076
Aakash-Ghosh08 <aakash.kumar.ghosh@gmail.com>
2026-01-22 16:39:28 -0800

    Add interactive camera controls and improve rendering
    Implemented keyboard controls in NoiseMapRunner for moving and turning the camera using WASD and IJKL keys. Refactored Renderer to support camera movement and rotation, updated screen rendering logic for real-time updates, and fixed issues in ray casting and screen update methods.

--- src/NoiseMapRunner.java
+++ src/NoiseMapRunner.java
@@ -1,6 +1,40 @@
 public class NoiseMapRunner {
 	public static void main(String[] args) {
-		
+		Renderer renderer = new Renderer(new NoiseMap(200, .5));
+		while (true) { 					
+			if(StdDraw.hasNextKeyTyped()) {
+				char key = StdDraw.nextKeyTyped();
+				if(key == 'w') {
+					renderer.moveUp(1);
+				}
+				if(key == 's') {
+					renderer.moveDown(1);
+				}
+				if(key == 'a') {
+					renderer.moveLeft(1);
+				}
+				if(key == 'd') {
+					renderer.moveRight(1);
+				}
+				if(key == 'i') {
+					renderer.turnUp(5);
+				}
+				if(key == 'k') {
+					renderer.turnDown(5);
+				}
+				if(key == 'j') {
+					renderer.turnLeft(5);
+				}
+				if(key == 'l') {
+					renderer.turnRight(5);
+				}
+			}
+			renderer.updateScreen();
+			renderer.renderScreen();
+		}
+
+
+		/*
 		StdDraw.enableDoubleBuffering();
 		for(int step = 0; step < 1000; step ++) {
 			double i = step/1000.0;
@@ -9,6 +43,7 @@ public class NoiseMapRunner {
 			System.out.println("Drew map with gradient " + i);
 			StdDraw.show();
 		}
+		*/
 		
 		/*
 		StdDraw.enableDoubleBuffering();
--- src/Renderer.java
+++ src/Renderer.java
@@ -21,28 +21,82 @@ public class Renderer {
 		screen = new double[(int)xRESOLUTION][(int)yRESOLUTION];
 	}
 
-	public void renderScreen() {
-
+	public void moveUp(double distance) {
+		this.z += distance;
 	}
-	
-	public void updateScreen(double x, double y, double xDir, double yDir) {
-		for(int i = (int) -xRESOLUTION/2; i < (int) xRESOLUTION/2; i++) {
-			for(int j = (int) -yRESOLUTION/2; j < (int) xRESOLUTION/2; j++) {
-				
+
+	public void moveDown(double distance) {
+		 this.z -= distance;
+	 }
+
+	public void moveLeft(double distance) {
+		this.x -= distance;
+	}
+
+	public void moveRight(double distance) {
+		this.x += distance;
+	}
+
+	public void turnLeft(double angle) {
+		this.xDir -= angle;
+	}
+
+	public void turnRight(double angle) {
+		this.xDir += angle;
+	}
+
+	public void turnUp(double angle) {
+		this.yDir += angle;
+	}
+
+	public void turnDown(double angle) {
+		this.yDir -= angle;
+	}
+
+	public void renderScreen() {
+		StdDraw.enableDoubleBuffering();
+		for(int i = 0; i < xRESOLUTION; i++) {
+			for(int j = 0; j < yRESOLUTION; j++) {
+				int shade = (int)(255 * screen[i][j]);
+				if(shade > 255) {
+					shade = 255;
+				}
+				StdDraw.setPenColor(shade, shade, shade);
+				StdDraw.setPenRadius(1.0 / xRESOLUTION);
+				StdDraw.point((double)i/xRESOLUTION + (double)1/(xRESOLUTION * 2), (double)j/yRESOLUTION + (double)1/(yRESOLUTION * 2));
 			}
 		}
+		StdDraw.show();
 	}
 	
-	public double castRay(double x, double y, double xDir, double yDir, int screenX, int screenY) {
-		Ray ray = new Ray(x, y, z, xDir, yDir);
-		double minDistance = 10
-		for(int i = 0; i < terrain.getResolution(); i++) {
-			for(int j = 0; j < terrain.getResolution(); j++) {
-				double t = ray.collidingWithBox(i, i + 1, j, j + 1, 0, terrain.getHeightAt(i, j);
-				if(t != -1) {
-					if(minDistance = 0)
+	public void updateScreen() {
+		for(int i = (int) -xRESOLUTION/2; i < (int) xRESOLUTION/2; i++) {
+			for(int j = (int) -yRESOLUTION/2; j < (int) yRESOLUTION/2; j++) {
+				double distance = castRay((FOV * i/xRESOLUTION + xDir), (FOV * j/yRESOLUTION + yDir));
+				if (distance == Double.POSITIVE_INFINITY || distance <= 0) {
+					screen[i + (int)xRESOLUTION/2][j + (int)yRESOLUTION/2] = 0;
+				}
+				else {
+					screen[i + (int)xRESOLUTION/2][j + (int)yRESOLUTION/2] = 1.0 / distance;
 				}
 			}
 		}
+	}
+	
+	public double castRay(double xDirection, double yDirection) {
+		Ray ray = new Ray(x, y, this.z, xDirection, yDirection);
+		double minDistance = 100000;
+		for(int i = 0; i < terrain.getResolution(); i++) {
+			for(int j = 0; j < terrain.getResolution(); j++) {
+				double t = ray.collidingWithBox(i, i + 1, j, j + 1, 0, terrain.getHeightAt(i, j));
+				if(t != -1) {
+					double distance = ray.getDistance(t);
+					if(distance < minDistance) {
+						minDistance = distance;
+					}
+				}
+			}
+		}
+		return minDistance;
 	}
 }
